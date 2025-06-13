133c6837774ef335fcb945a429212f54a555e1b3
codewizard <103335877+shiva2224@users.noreply.github.com>
2025-06-13 14:15:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello from Dockerized Node.js app!");
 });
 
-console.log("hello from build")
+console.log("hello from build!!!!!!!!123123")
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
