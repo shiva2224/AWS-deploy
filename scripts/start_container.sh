3476c9f708a199379b3117e9d1557ad71219de8b
codewizard <103335877+shiva2224@users.noreply.github.com>
2025-06-20 01:45:47 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull shivadocker24/nodejsbuildsample
 
 # Run the Docker image as a container
-docker run -d -p 3000:3000 shivadocker24/nodejsbuildsample
+docker run -d -p 8000:8000 shivadocker24/nodejsbuildsample
