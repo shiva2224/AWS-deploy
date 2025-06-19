097e93dc306f0f8c5728ff7622a12dfcd960ec1b
codewizard <103335877+shiva2224@users.noreply.github.com>
2025-06-20 01:43:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Dockerized Node.js app!");
